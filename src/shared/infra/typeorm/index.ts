bd7ce338ac3fbf8587347c8ad421075051820f61
Vinicius de Alcantra Barros <viniciusalcantarabarros@gmail.com>
2021-11-19 22:25:15 -0300

    Exportando configuraçao do banco de dados

---
+++ src/shared/infra/typeorm/index.ts
@@ -0,0 +1,12 @@
+import { Connection, createConnection, getConnection, getConnectionOptions } from "typeorm";
+
+export default async(host = "localhost"): Promise<Connection> => {
+    const defaultOptions = await getConnectionOptions()
+
+    return createConnection(
+        Object.assign(defaultOptions, {
+            host,
+            database: "eventour"
+        })
+    )
+}
